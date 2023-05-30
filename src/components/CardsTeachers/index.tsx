bffc72e297c9a794db8a46a59e05b31b9196cd79
philos-tec <suporte@philosgs.com>
2023-05-29 23:22:08 -0300

    Subindo alteracoes do card

--- src/components/CardsTeachers/index.tsx
+++ src/components/CardsTeachers/index.tsx
@@ -1,9 +1,33 @@
 import * as Local from "./styles";
+import diego from "../../assets/Diego.png";
 
 const CardsTeachers = () => {
   return (
     <Local.Container>
-      <Local.Content>{<Local.Icon></Local.Icon>}</Local.Content>
+      <Local.Content>
+        <Local.Header>
+          <Local.Icon src={diego} />
+          <Local.ContentHeader>
+            <Local.NameTeacher>Diego</Local.NameTeacher>
+            <Local.Theme>Química</Local.Theme>
+          </Local.ContentHeader>
+        </Local.Header>
+
+        <Local.Description>
+          Instrutor de Educação Física para iniciantes, minha missão de vida é
+          levar saúde e contribuir para o crescimento de quem se interessar.
+          <br />
+          <br />
+          Comecei a minha jornada profissional em 2001, quando meu pai me deu
+          dois alteres de 32kg com a seguinte condição: "Aprenda a fazer
+          dinheiro com isso!"
+        </Local.Description>
+        <Local.Footer>
+          <Local.Price>Preço/hora</Local.Price>
+          <Local.Value>R$ 40,00</Local.Value>
+          <Local.Button>Entrar em contato</Local.Button>
+        </Local.Footer>
+      </Local.Content>
     </Local.Container>
   );
 };
--- src/components/CardsTeachers/styles.ts
+++ src/components/CardsTeachers/styles.ts
@@ -12,8 +12,91 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   display: flex;
+  flex-direction: column;
 `;
-export const Icon = styled.img``;
-export const NameTeacher = styled.p``;
-export const Description = styled.p``;
-export const Value = styled.p``;
+
+export const Header = styled.div`
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: 30px;
+  margin-top: 40px;
+`;
+
+export const ContentHeader = styled.div`
+  width: 234px;
+  padding: 0;
+`;
+export const Icon = styled.img`
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  margin-right: 20px;
+`;
+export const NameTeacher = styled.p`
+  margin-bottom: -7px;
+  font-family: "Archivo", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  color: #32264d;
+`;
+export const Theme = styled.p`
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  color: #6a6180;
+`;
+export const Description = styled.p`
+  margin: 30px 30px 50px 30px;
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #6a6180;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  height: 120px;
+  align-items: center;
+  background: #fafafc;
+  border-radius: 0px 0px 8px 8px;
+  border-top: 1px solid #e6e6f0;
+`;
+
+export const Price = styled.p`
+  margin-left: 30px;
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #9c98a6;
+`;
+export const Value = styled.p`
+  margin-left: 20px;
+  font-family: "Archivo", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  color: #8257e5;
+`;
+export const Button = styled.button`
+  width: 250px;
+  height: 56px;
+  background: #04d361;
+  border: none;
+  border-radius: 8px;
+  margin-left: 37%;
+  font-family: "Archivo", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 26px;
+  color: #ffffff;
+`;
